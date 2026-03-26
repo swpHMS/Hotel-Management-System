c8544e6e002a1cc9fc120a9cf1d11fcb174698bf
meridabui <huyendieubui2005@gmail.com>
2026-03-26 21:17:15 +0700

    active Policy -admin
    active Policy -admin

--- src/java/controller/admin/policy/AdminPolicyServlet.java
+++ src/java/controller/admin/policy/AdminPolicyServlet.java
@@ -27,7 +27,6 @@ public class AdminPolicyServlet extends HttpServlet {
             request.setAttribute("activeKey", "");
             request.setAttribute("selectedPolicy", null);
             request.setAttribute("active", "system");
-            
             request.getRequestDispatcher("/view/admin/policy_list.jsp").forward(request, response);
             return;
         }
@@ -46,8 +45,7 @@ public class AdminPolicyServlet extends HttpServlet {
         request.setAttribute("policies", policies);
         request.setAttribute("activeKey", key);
         request.setAttribute("selectedPolicy", selected);
-        request.setAttribute("active", "system_config");
-
+       request.setAttribute("active", "system");
         request.getRequestDispatcher("/view/admin/policy_list.jsp").forward(request, response);
     }
 
