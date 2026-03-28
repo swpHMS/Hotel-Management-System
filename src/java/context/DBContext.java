ea8b9508c985ba916fa6fa45a80c2f31e855d4da
MaiNN63 <nguyenngocmai0603@gmail.com>
2026-03-28 21:44:33 +0700

    emove redundant customer servlets and isolate edit-profile form state

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -11,9 +11,9 @@ public class DBContext {
     public DBContext() {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=Hotel_Management_System2;encrypt=true;trustServerCertificate=true;";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=Hotel_Management_System3;encrypt=true;trustServerCertificate=true;";
             String username = "sa";
-            String password = "sa";
+            String password = "123";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
